77dd7d890a385d9a459ed6dcd904ee8117233889
jrvla <vladimir.artemj@gmail.com>
2026-01-29 01:12:50 +0500

    added redis service as a memory storage for sessions

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,54 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const cors = require("cors");
-const connectDb = require('./config/db.js');
-const session = require('express-session');
-require('dotenv').config();
+const connectDb = require("./config/db.js");
+const session = require("express-session");
+const { RedisStore } = require("connect-redis");
+const redis = require("redis");
+
+require("dotenv").config();
 const SESSION_SECRET = process.env.SESSION_SECRET;
+const REDIS_URL = process.env.REDIS_URL;
 
 connectDb();
 
-const authRoutes = require('./routes/authRoutes');
+const redisClient = redis.createClient({
+  url: REDIS_URL,
+});
+
+redisClient.connect().catch(console.error);
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     methods: "*",
-    credentials: true
-}))
-app.use(session({
-  name: 'sid',
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    sameSite: "lax",
-    maxAge: 1000 * 60 * 60
-  }
-}));
+    credentials: true,
+  }),
+);
+app.use(
+  session({
+    name: "sid",
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60,
+    },
+  }),
+);
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
